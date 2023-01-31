0ae529f09089713a74e62d093cb01b0c06031d20
Gaurav <hey9aurav@gmail.com>
2023-01-31 17:52:42 +0530

    Added extra functions to User Controller.

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,3 +22,81 @@ exports.getUsers = function (req, res) {
         }
     });
 };
+
+// Add User
+/*
+PARAMETERS :
+{
+    "param": {
+        "username": "",
+        "name": "",
+        "phone": ,
+        "email": "",
+        "sponsor_id": "",
+        "password": ""
+    }
+} 
+*/
+exports.addUser = function (req, res) {
+    var param = req.body.param;
+    sql.connect(serverConfig, function (err) {
+        if (err) console.error(err);
+        else {
+            var request = new sql.Request();
+            request.input("username", sql.VarChar, param.username);
+            request.input("name", sql.VarChar, param.name);
+            request.input("phone", sql.VarChar, param.phone);
+            request.input("email", sql.VarChar, param.email);
+            request.input("sponsor_id", sql.VarChar, param.sponsor_id);
+            request.input("password", sql.VarChar, param.password);
+            request.output('Message', sql.NVarChar(sql.MAX))
+            request.execute("AddUser", function (err, result) {
+                if (err) {
+                    console.error(err);
+                    sql.close();
+                    return res.status(500).send(err);
+                } else {
+                    sql.close();
+                    console.info(result.output.Message)
+                    return res.status(200).send(result.recordset);
+                }
+            });
+        }
+    });
+};
+
+// Change Password
+/*
+PARAMETERS :
+{
+    "param": {
+        "username": "",
+        "OldPassword": "",
+        "NewPassword": ""
+    }
+}
+*/
+exports.changeUserPassword = function (req, res) {
+    var param = req.body.param;
+    sql.connect(serverConfig, function (err) {
+        if (err) console.error(err);
+        else {
+            var request = new sql.Request();
+            request.input("username", sql.VarChar, param.username);
+            request.input("OldPassword", sql.VarChar, param.OldPassword);
+            request.input("NewPassword", sql.VarChar, param.NewPassword);
+            request.output('Message', sql.NVarChar(sql.MAX))
+            request.execute("ChangeUserPassword", function (err, result) {
+                if (err) {
+                    console.error(err);
+                    sql.close();
+                    return res.status(500).send(err);
+                } else {
+                    sql.close();
+                    console.info(result.output.Message)
+                    return res.status(200).send(result.recordset);
+                }
+            });
+        }
+    });
+};
--- routes/route.js
+++ routes/route.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var userController = require('../controllers/userController');
 
 router.post("/api/GetUsers", userController.getUsers);
+router.post("/api/AddUser", userController.addUser);
+router.post("/api/ChangeUserPassword", userController.changeUserPassword);
 
 module.exports = router;
