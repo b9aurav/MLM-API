f79ae111777a86b75c3b5dc52dbe51af091887f9
Gaurav <hey9aurav@gmail.com>
2023-07-06 12:12:13 +0530

    Autopool Team API Implemented.

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -72,3 +72,71 @@ exports.getTeamByLevel = function (req, res) {
         }
     });
 };
+
+// Get Autopool Team
+/*
+PARAMETERS :
+{
+    "param": {
+        "user_id": "",
+        "level": ,
+    }
+}
+*/
+exports.getAutopoolTeamByLevel = async function (req, res) {
+    try {
+      const param = req.body.param;
+      await sql.connect(serverConfig);
+      const request = new sql.Request();
+      request.input('user_id', sql.VarChar, param.user_id);
+      request.input('level', sql.VarChar, param.level);
+      request.output('memberCount', sql.NVarChar(10))
+      request.output('Message', sql.NVarChar(sql.MAX))
+      const result = await request.execute("GetAutopoolMemberCountsByLevel");
+      sql.close();
+  
+      console.info(result.output.Message);
+      return res.status(200).send({
+        message: result.output.Message,
+        data: result.recordset,
+        count: result.output.memberCount
+      });
+    } catch (err) {
+      console.error(err);
+      sql.close();
+      return res.status(500).send(err);
+    }
+  };
+  
+// Get Autopool Team Members
+/*
+PARAMETERS :
+{
+    "param": {
+        "user_id": "",
+        "level": ,
+    }
+}
+*/
+exports.getAutopoolMembers = async function (req, res) {
+    try {
+      const param = req.body.param;
+      await sql.connect(serverConfig);
+      const request = new sql.Request();
+      request.input('user_id', sql.VarChar, param.user_id);
+      request.output('Message', sql.NVarChar(sql.MAX))
+      const result = await request.execute("GetAutopoolMembers");
+      sql.close();
+  
+      console.info(result.output.Message);
+      return res.status(200).send({
+        message: result.output.Message,
+        data: result.recordset,
+        count: result.output.memberCount
+      });
+    } catch (err) {
+      console.error(err);
+      sql.close();
+      return res.status(500).send(err);
+    }
+  };
